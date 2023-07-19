6c00b33215e0b190bd7a5c2ce18bb7871085d081
Woozy <106052832+anfrosus@users.noreply.github.com>
2023-07-19 18:23:16 +0900

    충돌내기

--- src/main/java/com/example/rebasetest/Init.java
+++ src/main/java/com/example/rebasetest/Init.java
@@ -1,7 +1,8 @@
 package com.example.rebasetest;
 
 public class Init {
-    String msg = "최초의 상태입니다. 다른사람이 수정했습니다.";
+    String msg = "최초의 상태입니다. 내가 수정했습니다.";
     String msg1 = "다른사람의 작업입니다.";
     String msg2 = "나의 작업입니다.";
+    String msg3 = "변경3";
 }
